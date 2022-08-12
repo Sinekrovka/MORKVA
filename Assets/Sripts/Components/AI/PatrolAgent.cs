24b4233fc0c4beecaff1eb1074187be4f6248f9b
Sinekrovka <77225026+Sinekrovka@users.noreply.github.com>
2022-08-13 00:13:48 +0600

    Случайное передвижение Navmesh
    Осталась шкала шума и проигрыш

--- Assets/Sripts/Components/AI/PatrolAgent.cs
+++ Assets/Sripts/Components/AI/PatrolAgent.cs
@@ -1,11 +1,32 @@
 
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PatrolAgent : MonoBehaviour
 {
     private Transform levelPalete;
+    private NavMeshAgent _navMeshAgent;
+    private bool stop;
     private void Awake()
     {
-        levelPalete = LevelGenerator.Instance.GetLevelContainer;
+        levelPalete = LevelGenerator.Instance.GetLevelContainer.GetChild(0);
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+       SetRandomPoint();
     }
+
+    private void Update()
+    {
+        if (_navMeshAgent.stoppingDistance >= Vector3.Distance(_navMeshAgent.destination, transform.position))
+        {
+            SetRandomPoint();
+        }
+    }
+
+    private void SetRandomPoint()
+    {
+        Transform randString = levelPalete.GetChild(Random.Range(0, levelPalete.childCount));
+        _navMeshAgent.SetDestination(randString.GetChild(Random.Range(0, randString.childCount)).position);
+    }
+    
+    
 }
